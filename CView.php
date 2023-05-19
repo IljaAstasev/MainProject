458fb4dfe5a340e3686c7a07a483c0652e5b1d72
IljaAstasev <79198863+IljaAstasev@users.noreply.github.com>
2023-05-19 13:19:23 +0300

    Add files via upload

---
+++ CView.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once ('./controllers/CController.php');
+
+class CView {
+    public function ViewMainPage(){
+        $controller = new CController();
+        $arrayAll = $controller->SetMainPage();
+        include 'views/view_mainPage.php';
+        return $arrayAll;
+    }
+
+    // public function ViewAirForce(){
+    //     $controller = new CController();
+    //     $arrayAll = $controller->SetAirForce();
+    //     include 'views/view_airforce.php';
+    //     return $arrayAll;
+    // }
+
+    // public function ViewError404(){
+    //     $controller = new CController();
+    //     $arrayAll = $controller->SetError404();
+    //     include 'views/view_error404.php';
+    //     return $arrayAll;
+    // }
+
+
+    // public function GetFooter(){
+    //     return 'views/footer.php';
+    // }
+
+}
